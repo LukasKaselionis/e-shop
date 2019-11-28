d814fb3c709d909d1b1aa1630275440a166739b6
LukasKaselionis <l.kaselionis@gmail.com>
2019-11-28 21:45:24 +0200

    taking products data with ajax and saving in session

---
+++ app/Http/Controllers/Front/CartController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        return view('front.cart');
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function add(Request $request)
+    {
+        dd(session()->get('cart'));
+        $productId = $request->input('productId');
+        $productPrice = $request->input('productPrice');
+        $productName = $request->input('productName');
+        if (!session()->exists('cart.' . $productId)) {
+            session()->put('cart.' . $productId, [
+                'product_id' => $productId,
+                'quantity' => 0,
+                'name' => $productName,
+                'price' => $productPrice
+            ]);
+        }
+        session()->increment('cart.' . $productId . '.quantity');
+        return 'ok';
+    }
+}
